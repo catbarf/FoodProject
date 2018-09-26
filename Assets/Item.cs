d21313417a30827efc09e059cdad7696c17a2b0b
catbarf <richard.andersen@elev.ga.lbs.se>
2018-09-26 15:26:57 +0200

    Vad som helst

---
+++ Assets/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public string displayName;
+    public float weight;
+    public float cost;
+
+    public float GetPricePerWeight()
+    {
+        return cost / weight;
+    }
+
+    public override string ToString()
+    {
+        return "Item Name: " + displayName + "\ncost " + cost.ToString() + "\nWeight: " + weight.ToString() + "\nCost Perk Kilo " + GetPricePerWeight().ToString();
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log(ToString());
+    }
+}
---
+++ Assets/Usable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usable : Item {
+
+    public float uses;
+    public float maxUses;
+
+    public void OnMouseUpAsButton()
+    {
+        Use();
+    }
+
+    public void Use()
+    {
+        if (uses < maxUses)
+        {
+            uses++;
+        }
+    }
+
+    public string UseText()
+    {
+        return "You ate a " + ("") ;
+    }
+    
+    public string NoUsesRemainingText()
+    {
+        return "There are no uses remaining of this item.";
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
